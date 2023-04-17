a9c89d376181e78aeaa3f6bc31d93ab7e34bc50f
CaioXimendes <caioximendes1@gmail.com>
2023-04-17 10:25:23 -0300

    códigos criados em Python durante os exercicios das aulas do curso de lógica de programação do CDD 4.0

--- Aula 04(reposição)/project0.py
+++ Aula 04(reposição)/project0.py
@@ -1,13 +1,15 @@
 nota_01 = 0
 nota_02 = 0
-while nota_01 >= 0 and nota_01 <= 10 and nota_02 >=0 and nota_02 <=10:
+pergunta = "S"
+while pergunta == "S":
     nota_01 = float(input("Olá, Informe a nota da avaliacao 01 do aluno: \n"))
-    if nota_01 < 0 or nota_01 > 10:
+    while nota_01 < 0 and nota_01 > 10:
         print("Os valores da nota devem estar entre 0 em 10! Tente novamente!")
-        break
     nota_02 = float(input("Olá, Informe a nota da avaliacao 02 do aluno: \n"))
-    if nota_02 < 0 or nota_02 > 10:
+    while nota_02 < 0 and nota_02 > 10:
         print("Os valores da nota devem estar entre 0 em 10! Tente novamente!")
-        break
     media = (nota_01 + nota_02) / 2
     print("A média do aluno foi :", media)
+    pergunta = input("Deseja continuar? S/N para Sim ou Não \n")
+else:
+    print("Fim do programa!")
