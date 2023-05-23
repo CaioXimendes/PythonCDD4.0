8b495b10d73960550af86006fedaabede81f9c0b
Caio Ximendes <caioximendes1@gmail.com>
2023-05-23 08:32:36 -0300

    ajuste dos prints e alguns if e else, com os self.comendo e falando ajustados.

--- Aula 09/projeto0.py
+++ Aula 09/projeto0.py
@@ -7,54 +7,68 @@ class Pessoa:
         self.falando = falando
     def comer (self, comida):
        if self.comendo == True:
-           print(f"{self.nome} já foi comer e não precisa comer novamente")
+           if self.falando == True:
+               self.comendo = False
+               print(f"{self.nome} não pode comer e falar ao mesmo tempo!")
+           else:
+               self.comida = comida
+               self.comendo = True
+               print(f"{self.nome} começou a comer {self.comida}")
        else:
+           self.comendo = True
            self.comida = comida
-           print(f"{self.nome} foi comer {self.comida}")
+           print(f"{self.nome} começou a comer {self.comida}")
     def parardecomer(self,comida):
         if self.comendo == True:
-            self.comida = comida
-            self.comendo = False
-            print(f"{self.nome} estava comendo {self.comida} e parou de comer")
+            if self.falando == True:
+                self.comendo = True
+                print(f"{self.nome} não pode parar de comer pois já estava comendo")
+            else:
+                self.comendo = False
+                print(f"{self.nome} estava comendo e parou de comer")
         else:
-            print(f"{self.nome} não estava comendo")
+            self.comendo = False
+            print(f"{self.nome} não pode parar de comer pois não esta comendo")
     def falar(self):
         if self.falando == True:
-            if self.comendo == False:
-                print(f"{self.nome} já está falando e não pode falar novamente")
-            else:
-                self.falando = False
-                print(f"{self.nome} não pode falar pois já está comendo!")
-        else:
             if self.comendo == True:
-                print(f"{self.nome} não esta falando e não pode falar enquanto come")
+                self.falando = False
+                print(f"{self.nome} não pode falar enquanto come")
             else:
                 self.falando = True
-                print(f"{self.nome} não esta falando e nem comendo e começou a falar")
+                print(f"{self.nome} começou a falar!")
+        else:
+            if self.comendo == True:
+                self.falando = False
+                print(f"{self.nome} não pode falar enquanto come")
+            else:
+                self.falando = True
+                print(f"{self.nome} começou a falar!")
     def parardefalar(self):
         if self.falando == True:
             if self.comendo == True:
                 self.falando = False
-                print(f"{self.nome} não está falando pois ainda está comendo ")
+                print(f"{self.nome} não pode parar de falar pois está comendo e não falando ")
             else:
+                self.falando = False
                 print(f"{self.nome} parou de falar e não estava comendo")
         else:
             if self.comendo == True:
                 self.falando = False
-                print(f"{self.nome} Não está falando e está comendo")
+                print(f"{self.nome} Não pode parar de falar pois está comendo e não está falando")
             else:
-                print(f"{self.nome} Parou de falar e também não estava comendo")
+                print(f"{self.nome} Não precisa parar de falar, pois já estava sem falar e sem comer")
 
 p1 = Pessoa("Joao", 80 , 23)
 p2 = Pessoa("maria",54,19, True)
 # print(p1.nome)
 # print(vars(p1))
 # print(vars(p2))
-p1.comer("banana")
+#p1.comer("banana")
 p2.comer("maça")
-p1.parardecomer("banana")
+#p1.parardecomer("banana")
 p2.parardecomer("banana")
-p1.falar()
+#p1.falar()
 p2.falar()
-p1.parardefalar()
+#p1.parardefalar()
 p2.parardefalar()
