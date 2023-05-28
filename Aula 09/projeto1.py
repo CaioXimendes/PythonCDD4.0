fb5ec94567e597a8c733bd433b904b9e4c9f8949
Caio Ximendes <caioximendes1@gmail.com>
2023-05-27 23:41:24 -0300

    Saque, deposito em funcionamento, versão 1.0

--- Aula 09/projeto1.py
+++ Aula 09/projeto1.py
@@ -1,5 +1,5 @@
 class ContaBancaria:
-    def __init__ (self,num_conta,saldo, nome_cliente, tipo_conta, limite,status = False):
+    def __init__(self, num_conta, saldo, nome_cliente, tipo_conta, limite, status=False):
         self.num_conta = num_conta
         self.saldo = saldo
         self.nome_cliente = nome_cliente
@@ -19,23 +19,61 @@ class ContaBancaria:
             if self.limite != 0:
                 print("Não é possivel desativar um limite diferente de 0")
             else:
-                print("Seu limite agora está desativado!")
                 self.limite = 0
+                print("Seu limite agora está desativado!")
         else:
             print("Não é possível desativar o limite de uma conta desativada!")
 
-    def depositar(self,deposito):
+    def depositar(self, deposito):
         if self.status == True:
-            self.saldo += deposito
+            if self.limite != 0:
+                if self.saldo <= 0:
+                    valor_depositado_no_limite = deposito + self.saldo
+                    self.saldo = 0
+                    valor_depositado_no_saldo = valor_depositado_no_limite + self.saldo
+                    self.saldo = valor_depositado_no_saldo
+                    print(f"Você depositou R${deposito}")
+                else:
+                    self.saldo += deposito
+                    print(f"Você depositou R${deposito}")
+            else:
+                self.saldo += deposito
+                print(f"Você depositou R${deposito}")
         else:
             print("Você não pode depositar em uma conta inativa!")
     def sacar(self,saque):
-        if saque > self.saldo:
-            novo_saldo = (saque - self.saldo) - (saque - self.limite)
-            self.saldo = novo_saldo
-            print(f"você sacou R$ {saque}")
+        if self.status == True:
+            if self.limite != 0:
+                if saque > self.saldo:
+                    valor_retirado = saque - self.saldo
+                    valor_retirado_do_limite = self.limite - valor_retirado
+                    self.saldo = valor_retirado_do_limite - self.limite
+                    print(f"você sacou R$ {saque}")
+                else:
+                    self.saldo -= saque
+                    print(f"você sacou R$ {saque}")
+
+            elif self.limite == 0 and saque > self.saldo:
+                valor_retirado_do_saldo = saque - self.saldo
+                valor_sacado_sem_limite = saque - valor_retirado_do_saldo
+                self.saldo -= valor_sacado_sem_limite
+                print(f"você sacou R$ {valor_sacado_sem_limite} e o valor que queria sacar {saque}")
+
+            elif self.limite == 0 and self.saldo > saque:
+                self.saldo -= saque
+                print(f"você sacou R$ {saque}")
+
+            elif self.limite == 0 and self.saldo == saque:
+                valor_retirado_do_saldo = self.saldo - saque
+                valor_sacado_sem_limite = saque - valor_retirado_do_saldo
+                self.saldo -= valor_sacado_sem_limite
+                print(f"você sacou R$ {valor_sacado_sem_limite}")
+        else:
+            print("Você não pode sacar em uma conta desativada!")
+
     def verificarsaldo (self):
         if self.status == True:
+
             print(f"Seu saldo atual é de: R${self.saldo}")
         else:
             print(f"Não é possível verificar o saldo de uma conta inativa!")
@@ -54,11 +92,13 @@ class ContaBancaria:
         else:
             print("Não é possível desativar uma conta que já está desativada!")
 
-conta1 = ContaBancaria(123456789,800,"caio","corrente",1000,False)
+conta1 = ContaBancaria(123456789,4,"caio","corrente",0,True)
 
 print(conta1.tipo_conta)
 conta1.ativarconta()
-conta1.ativarlimite()
+conta1.desativarlimite()
 conta1.verificarsaldo()
-conta1.sacar(1000)
+conta1.sacar(5)
+conta1.verificarsaldo()
+conta1.depositar(450)
 conta1.verificarsaldo()
