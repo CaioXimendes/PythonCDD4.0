6b9276c65c5a8b5ff1975a04b5d00281d66cd6f3
CaioXimendes <caioximendes1@gmail.com>
2023-05-31 11:26:39 -0300

    correções sobre classes realizadas

--- Aula 09/projeto5.py
+++ Aula 09/projeto5.py
@@ -2,6 +2,7 @@ class Atleta:
     def __init__(self,aposentado,peso):
         self.aposentado = aposentado
         self.peso = peso
+        self.aquecido = False
     def Aposentar(self):
         if self.aposentado == True:
             print("O atleta já está aposentado! Não pode aposentar novamente!")
@@ -11,6 +12,7 @@ class Atleta:
         if self.aposentado == True:
             print("O atleta não pode se aquecer, pois está aposentado!")
         else:
+            self.aquecido = True
             print("O atleta está aquecendo!")
 
 class Corredor(Atleta):
@@ -20,7 +22,10 @@ class Corredor(Atleta):
         if self.aposentado == True:
             print("O Corredor não pode correr, pois está aposentado")
         else:
-            print("O Corredor começou a correr!")
+            if self.aquecido == True:
+                print("O Corredor começou a correr!")
+            else:
+                print(f"O corredor não pode correr sem estar aquecido!")
 class Nadador(Atleta):
     def __init__(self,aposentado,peso):
         super().__init__(aposentado,peso)
@@ -28,7 +33,10 @@ class Nadador(Atleta):
         if self.aposentado == True:
             print("O Nadador não pode nadar, pois está aposentado!")
         else:
-            print("O Nadador começou a nadar!")
+            if self.aquecido == True:
+                print("O Nadador começou a nadar!")
+            else:
+                print("O Nadador não pode nadar sem estar aquecido!")
 class Ciclista(Atleta):
     def __init__(self,aposentado,peso):
         super().__init__(aposentado,peso)
@@ -36,7 +44,10 @@ class Ciclista(Atleta):
         if self.aposentado == True:
             print("O Ciclista não pode pedalar, pois está aposentado!")
         else:
-            print("O Ciclista começou a pedalar!")
+            if self.aquecido == True:
+                print("O Ciclista começou a pedalar!")
+            else:
+                print(f"O ciclista não pode pedalar sem estar aquecido!")
 
 class Triatleta(Corredor,Nadador,Ciclista):
     def __init__ (self, aposentado,peso):
@@ -46,4 +57,5 @@ triatleta1 = Triatleta(True,60)
 triatleta1.Correr()
 
 triatleta2 = Triatleta(False,80)
+triatleta2.Aquecer()
 triatleta2.Pedalar()
